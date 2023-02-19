230fc779ee1fed2103d272b9600927b8faa1b9ec
Mohamed Hassan <mohamed.hassan@pmagentur.com>
2023-02-19 18:06:37 +0100

    updated icons story

---
+++ projects/fews/src/lib/components/pm-icon/pm-icon.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+export enum IconColor {
+  PRIMARY = 'primary',
+}
+
+export enum Icon {
+  FAVORITE = 'favorite',
+  SORT = 'sort',
+  SEARCH = 'search',
+  CART = 'cart',
+  STAR_FILLED = 'star_filled',
+  STAR_EMPTY = 'star_empty',
+  REFRESH = 'refresh',
+  FILTER = 'filter',
+}
+
+@Component({
+  selector: 'pm-icon',
+  templateUrl: './pm-icon.component.html',
+  styleUrls: ['./pm-icon.component.scss'],
+})
+export class PmIconComponent implements OnInit {
+  @Input()
+  size: number = 48;
+
+  @Input()
+  rotate: number = 0;
+
+  @Input()
+  weight: number = 300;
+
+  @Input()
+  color: IconColor = IconColor.PRIMARY;
+
+  @Input()
+  icon: Icon = Icon.FAVORITE;
+
+  /**
+   * @ignore
+   */
+  readonly EIconColor: typeof IconColor = IconColor;
+
+  constructor() {}
+
+  /**
+   * @ignore
+   */
+  ngOnInit(): void {}
+}
---
+++ stories/components/icon/data.ts
@@ -0,0 +1,7 @@
+import { IconColor, Icon } from '../../../projects/fews/src/lib/components/pm-icon/pm-icon.component';
+
+export const IconData = {
+    color: IconColor.PRIMARY,
+    icon: Icon.FAVORITE,
+    size: 40,
+}
