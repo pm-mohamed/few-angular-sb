9647642ce1eb92a5847ac7bfafe1e5c45e6947c9
Mohamed Hassan <mohamed.hassan@pmagentur.com>
2023-03-01 15:08:25 +0100

    added img preview files

---
+++ projects/fews/src/lib/layouts/pm-imagepreview/pm-imagepreview.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'pm-imagepreview',
+  templateUrl: './pm-imagepreview.component.html',
+  styleUrls: ['./pm-imagepreview.component.scss']
+})
+export class PmImagepreviewComponent implements OnInit {
+
+  constructor() { }
+
+  /**
+   * @ignore
+   */
+  ngOnInit(): void {
+  }
+}
---
+++ stories/layouts/pm-imagepreview/data.ts
@@ -0,0 +1,4 @@
+export const ImagepreviewData = {
+    mainImg: 'https://res.cloudinary.com/dqlwspssr/image/upload/v1637664626/coding-case/1_zbsprf.png'
+  };
+  
