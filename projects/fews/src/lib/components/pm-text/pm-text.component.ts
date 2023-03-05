bfa3f9a25791014767bb9c95498552ebd563f14b
Mohamed Hassan <mohamed.hassan@pmagentur.com>
2023-03-05 20:14:35 +0100

    copy-m-bold

--- projects/fews/src/lib/components/pm-text/pm-text.component.ts
+++ projects/fews/src/lib/components/pm-text/pm-text.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export enum TextType {
   H4 = "h4",
   COPY_L_BOLD = "copy-l-bold",
+  COPY_M_BOLD_PRIMARY = "copy-m-bold-primary",
+  COPY_M_BOLD_SECONDARY = "copy-m-bold-secondary",
   COPY_S_MEDIUM_PRIMARY = "copy-s-medium-primary",
   COPY_S_MEDIUM_SECONDARY = "copy-s-medium-secondary",
 }
